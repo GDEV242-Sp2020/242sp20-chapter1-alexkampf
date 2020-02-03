6425b27615c4ea5ca637775ad09a27f74ad45a0e
alexkampf <alexk764@gmail.com>
2020-02-03 02:45:31 -0500

    Update Picture.java

--- Picture.java
+++ Picture.java
@@ -1,20 +1,29 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ *  The picture being created in this code represents a person enjoying a night
+ *  sailing on their boat.
  *
- * This class was written as an early example for teaching Java with BlueJ.
+ * A version of this class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author  Alexander Kampf
+ * @version 2020.01.30
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
-    private Circle sun;
+    private Square sky1;
+    private Square sky2;
+    private Square sky3;
+    private Square boat1;
+    private Square boat2;
+    private Square boat3;
+    private Square boat4;
+    private Square boat5;
+    private Square ocean1;
+    private Square ocean2;
+    private Square sailPole;
+    private Square sail2;
+    private Triangle sail1;
+    private Person person;
+    private Circle moon;
     private boolean drawn;
 
     /**
@@ -22,10 +31,21 @@ public class Picture
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
-        sun = new Circle();
+        sky1 = new Square();
+        sky2 = new Square();
+        sky3 = new Square();
+        boat1 = new Square();
+        boat2 = new Square();
+        boat3 = new Square();
+        boat4 = new Square();
+        boat5 = new Square();
+        ocean1 = new Square();
+        ocean2 = new Square();
+        sail2 = new Square();
+        sailPole = new Square();
+        sail1 = new Triangle();
+        person = new Person();
+        moon = new Circle();
         drawn = false;
     }
 
@@ -35,28 +55,91 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.changeColor("blue");
-            wall.makeVisible();
+            sky1.moveHorizontal(-320);
+            sky1.moveVertical(-180);
+            sky1.changeSize(255);
+            sky1.changeColor("black");
+            sky1.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
+            sky2.changeColor("black");
+            sky2.moveHorizontal(-50);
+            sky2.moveVertical(-180);
+            sky2.changeSize(255);
+            sky2.makeVisible();
+            
+            ocean1.changeColor("blue");
+            ocean1.moveHorizontal(-310);
+            ocean1.moveVertical(40);
+            ocean1.changeSize(500);
+            ocean1.makeVisible();
+            
+            sailPole.changeColor("white");
+            sailPole.changeSize(60);
+            sailPole.moveHorizontal(-65);
+            sailPole.moveVertical(40);
+            sailPole.makeVisible();
+            
+            ocean2.changeColor("blue");
+            ocean2.moveHorizontal(-50);
+            ocean2.moveVertical(40);
+            ocean2.changeSize(60);
+            ocean2.makeVisible();
+            
+            sail1.changeColor("red");
+            sail1.moveHorizontal(95);
+            sail1.moveVertical(-40);
+            sail1.makeVisible();
+          
+            person.moveHorizontal(-40);
+            person.moveVertical(25);
+            person.makeVisible();
+             
+            boat1.changeColor("white");
+            boat1.changeSize(50);
+            boat1.moveHorizontal(-100);
+            boat1.moveVertical(100);
+            boat1.makeVisible();
+            
+            boat2.changeColor("white");
+            boat2.changeSize(50);
+            boat2.moveHorizontal(-90);
+            boat2.moveVertical(100);
+            boat2.makeVisible();
+            
+            boat3.changeColor("white");
+            boat3.changeSize(50);
+            boat3.moveHorizontal(-65);
+            boat3.moveVertical(100);
+            boat3.makeVisible();
+            
+            boat4.changeColor("white");
+            boat4.changeSize(40);
+            boat4.moveHorizontal(-120);
+            boat4.moveVertical(100);
+            boat4.makeVisible();
+            
+            boat5.changeColor("white");
+            boat5.changeSize(40);
+            boat5.moveHorizontal(-35);
+            boat5.moveVertical(100);
+            boat5.makeVisible();
+            
+            sail2.changeColor("red");
+            sail2.moveHorizontal(-65);
+            sail2.moveVertical(-20);
+            sail2.makeVisible();
+            
+            sky3.changeColor("black");
+            sky3.moveHorizontal(-80);
+            sky3.moveVertical(-120);
+            sky3.changeSize(100);
+            sky3.makeVisible();
     
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
-    
-            sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
-            sun.makeVisible();
+            moon.changeColor("white");
+            moon.moveHorizontal(100);
+            moon.moveVertical(-40);
+            moon.changeSize(80);
+            moon.makeVisible();
             drawn = true;
         }
     }
@@ -66,10 +149,16 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
-        sun.changeColor("black");
+        ocean1.changeColor("white");
+        ocean2.changeColor("white");
+        boat1.changeColor("black");
+        boat2.changeColor("black");
+        boat3.changeColor("black");
+        boat4.changeColor("black");
+        boat5.changeColor("black");
+        sailPole.changeColor("black");
+        sail1.changeColor("white");
+        sail2.changeColor("white");
     }
 
     /**
@@ -77,9 +166,15 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
-        sun.changeColor("yellow");
+        ocean1.changeColor("blue");
+        ocean2.changeColor("blue");
+        boat1.changeColor("white");
+        boat2.changeColor("white");
+        boat3.changeColor("white");
+        boat4.changeColor("white");
+        boat5.changeColor("white");
+        sailPole.changeColor("white");
+        sail1.changeColor("red");
+        sail2.changeColor("red");
     }
 }
